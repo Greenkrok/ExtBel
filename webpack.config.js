5315db782e61a85c9123e0914ace727c4200f226
Greenkrok <hellsing_13@mail.ru>
2021-08-15 15:12:16 +0300

    refactor: create the structure of project and configure webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+/*jshint esversion: 6 */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const miniCss = require('mini-css-extract-plugin');
+const minify = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/scripts//main.js',
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.(js)$/,
+        use: 'babel-loader' },
+      { test: /.(s*)css$/,
+        use: [ miniCss.loader, 'css-loader?url=false', 'sass-loader' ] },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          minimize: true,
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  optimization: {
+		minimizer: [
+			new minify({})
+		],
+	},
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+          {
+            from: path.resolve(__dirname, 'src/img'),
+            to:   path.resolve(__dirname, 'dist/img')
+          }
+        ]
+      }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new miniCss({
+			filename: 'style.css',
+		}),
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+};
