345b38b9a14f657bd5c9cb62b3e8638dc43d500a
Greenkrok <hellsing_13@mail.ru>
2021-08-15 20:17:46 +0300

    feat: add the opening search on mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,4 @@
 import '../styles/reset.css';
 import '../styles/scss/style.scss';
 import './modal';
+import './mobileSearch';
---
+++ src/scripts/mobileSearch.js
@@ -0,0 +1,21 @@
+/*jshint esversion: 6 */
+
+if( window.innerWidth < 960 ) {
+  const $header = document.querySelector('header');
+  const $btnSearch = document.querySelector('.search-form__button-container');
+  const $btnClose = document.getElementsByClassName('close')[1];
+  const $inputSearch = document.querySelector('.search-form__input');
+
+  $btnSearch.addEventListener('click', function() {
+    $header.classList.add('mobile');
+    $inputSearch.placeholder='Search';
+  });
+
+
+// Закрытие окна
+const closeModal = () => {
+  $header.classList.remove('mobile');
+};
+
+$btnClose.addEventListener('click', closeModal);
+}
