00e08edb8a5c0a90bb1d3ea7b1b5930fd8ff1b30
Greenkrok <hellsing_13@mail.ru>
2021-08-17 21:18:03 +0300

    feat: install jquery and slick slider

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
-/*jshint esversion: 6 */
-
+// Libraries
 const path = require('path');
+const webpack = require("webpack");
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const miniCss = require('mini-css-extract-plugin');
 const minify = require('optimize-css-assets-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
+// Configuration
 module.exports = {
-  entry: './src/scripts//main.js',
+  entry: './src/scripts/main.js',
   module: {
     rules: [
       { test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ] },
+
       { test: /\.(js)$/,
         use: 'babel-loader' },
+        
       { test: /.(s*)css$/,
         use: [ miniCss.loader, 'css-loader?url=false', 'sass-loader' ] },
+
       {
         test: /\.html$/,
         loader: 'html-loader',
@@ -23,21 +28,25 @@ module.exports = {
           minimize: true,
         },
       },
+
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
     ]
   },
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
   },
+
   optimization: {
 		minimizer: [
 			new minify({})
 		],
 	},
+  
   plugins: [
     new CopyPlugin({
       patterns: [
@@ -47,12 +56,22 @@ module.exports = {
           }
         ]
       }),
+
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
+
     new miniCss({
 			filename: 'style.css',
 		}),
+
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.$": "jquery",
+        "window.jQuery": "jquery"
+    }),
   ],
+
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 };
