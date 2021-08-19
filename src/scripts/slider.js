e4020305e2325b74c61284479c42a370019c9300
Greenkrok <hellsing_13@mail.ru>
2021-08-19 19:47:53 +0300

    fix: slider and some styles

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
   $('.slider').slick({
     dots: true,
-    slidesToShow: 2,
     waitForAnimate: false,
+    mobileFirst: true,
+    autoplay:true,
     responsive: [
       {
-        breakpoint: 960,
-        setting: {
-          slidesToShow: 1
-        }
+          breakpoint: 960,
+          settings: {
+              slidesToShow: 2
+          }
+      },
+
+      {
+          breakpoint: 720,
+          settings: {
+              slidesToShow: 1
+          }
       }
-    ],
-    mobileFirst: true
+      ]
   });
 });
